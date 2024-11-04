8d628f8ce1ba35c15f1d281a1c85fa842b5ee30f
imad <imad.taibi2003@gmail.com>
2024-11-04 20:30:36 +0000

    set the flask env

---
+++ Classes/home_page.py
@@ -0,0 +1,9 @@
+class HomePage:
+    def __init__(self):
+        pass
+
+    def display_welcome_message(self):
+        return "Welcome to the Home Page!"
+
+    def get_featured_items(self):
+        return ["Pizza", "Burger", "Pasta"]
---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import Flask
+from classes.home_page import HomePage
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, Flask!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
