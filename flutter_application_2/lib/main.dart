fa3fbb98e30fa00af26bc293ecc56061f2573daa
N1128897 <N1128897@my.ntu.ac.uk>
2024-11-12 20:22:55 +0000

    Resolved merge conflict in main.dart

--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -2,15 +2,10 @@
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
 import 'register_screen.dart';
-<<<<<<< HEAD
 import 'chef_control_panel.dart'; // Import the new ChefControlPanel screen
-=======
-import 'chef_control_panel.dart';
-import 'User_Management.dart';
->>>>>>> 9f57868c874126f2a51b4f40d271ebff4818f6e9
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -27,12 +22,8 @@ class MyApp extends StatelessWidget {
       routes: {
         '/login': (context) => LoginScreen(),
         '/register': (context) => RegisterScreen(),
-<<<<<<< HEAD
         '/chef_control': (context) =>
             const ChefControlPanel(), // Add route for Chef Control Panel
-=======
-        '/chef_control': (context) => ChefControlPanel(),
->>>>>>> 9f57868c874126f2a51b4f40d271ebff4818f6e9
       },
     );
   }
